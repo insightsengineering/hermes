b23cd902b12d004f251bd93f6c458529f79421e5
Namrata Bhatia <bhatian3@gene.com>
2021-05-19 12:41:42 -0700

    draft 2: test failure fix pending

--- R/HermesData-methods.R
+++ R/HermesData-methods.R
@@ -1,73 +1,29 @@
+#' HermesData Methods
+#' 
+#' @description Combines HermesData objects with the same samples but differnet features of interest (rows in assays).
+#' 
+#' @param args HermesData
+#'
+#' @return A HermesData
+#' @export 
+#'
+#' @examples
+#' hd <- rbind(hd1, hd2)
+
 setMethod(f = "rbind", 
-          signature = c("SummarizedExperiment"),
+          "HermesData",
           function(..., deparse.level = 1)
           {
            args <- unname(list(...)) #removes names and dimnames of the object
-           .rbind.SummarizedExperiment(args)
+           .rbind.HermesData(args)
           })
 
 # rbind function ----
-.rbind.SummarizedExperiment <- function(args) 
+
+.rbind.HermesData <- function(args)
   {
-  SE_check <- unlist(lapply(args, function(x) "SummarizedExperiment" %in% class(x)))
-  if (!all(SE_check)) stop("Not all input objects are SummarizedExperiment, please check again")
-  
+  SE_check <- unlist(lapply(args, function(x) "HermesData" %in% class(x)))
+  if (!all(SE_check)) stop("Not all input objects are HermesData, please check again")
+
   SummarizedExperiment:::.rbind.SummarizedExperiment(args)
 }
-
-# Namrata's thought process ----
-# Step 1: created a R and test file using use_r() file and use_test().
-
-# Purpose: combine objects with same samples but different features of interest
-# SummarizeExperiment package has rbind function. Is the goal to mimic this functionality?
-
-# Trying to understand rnaseqtools package code ----
-# Unname(): removes names / dimnames of the object. Don't we want to keep the colnames per description of
-#    SummarizedExperiment::rbind()? "The colnames in rowData(SummarizedExperiment) must match or an error is thrown. 
-#                                   Duplicate columns of colData(SummarizedExperiment) must contain the same data."
-
-# General question: what does it mean to have a . in function name -- .rbind.SummarizedExperiment(args)?
-
-# How are they able to call .rbind.SummarizedExperiment function from SummarizedExperiment package? 
-# -- if we want to call this function, then should it just be SummarizedExperiment::rbind()? what is row 15 doing?
-
-# What is the rationale of adding a SE_check for class in the function vs adding it as a test in test_that?
-
-# To test my function as I go: should I create dummy SE? or should I be using a sample dataset from somewhere?
-# -- what should be the workflow here? Test only directly using test_that?
-
-# Roxygen comments -- where will they go in this methods file? All the way at the top of above rbind function?
-# -- when I add Roxygen skeleton it to rbind, it gets added right above rbind
-# 
-
-
-library(SummarizedExperiment)
-?SummarizedExperiment::`rbind,SummarizedExperiment-method`
-nrows <- 200; ncols <- 6
-counts <- matrix(runif(nrows * ncols, 1, 1e4), nrows)
-colData <- DataFrame(Treatment=rep(c("ChIP", "Input"), 3),
-                     row.names=LETTERS[1:6])
-se0 <- SummarizedExperiment(assays=SimpleList(counts=counts),
-                            colData=colData)
-se0
-dim(se0)
-dimnames(se0)
-assayNames(se0)
-head(assay(se0))
-assays(se0) <- endoapply(assays(se0), asinh)
-head(assay(se0))
-
-rowData(se0)
-colData(se0)
-
-se0[, se0$Treatment == "ChIP"]
-subset(se0, select = Treatment == "ChIP")
-
-## rbind() combines objects with the same samples but different
-## features of interest:
-se1 <- se0
-se2 <- se1[1:50,]
-rownames(se2) <- letters[seq_len(nrow(se2))]
-cmb2 <- rbind(se1, se2)
-dim(cmb2)
-dimnames(cmb2)
--- tests/testthat/test-HermesData-methods.R
+++ tests/testthat/test-HermesData-methods.R
@@ -1,3 +1,51 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# Create HermesData ----
+object <- SummarizedExperiment::SummarizedExperiment(
+  list(counts = matrix(1L, 2, 1)),
+  rowData = data.frame(
+    HGNC = c(1, 1),
+    GeneID = c(1, 1),
+    Chromosome = c(1, 1),
+    StartBP = c(1, 1),
+    EndBP = c(1, 1),
+    WidthBP = c(1, 1),
+    HGNCGeneName = c(1, 1),
+    CanonicalTranscript = c(1, 1),
+    ProteinTranscript = c(1, 1),
+    LowExpressionFlag = c(1, 1)
+  ),
+  colData = data.frame(
+    SampleID = 1,
+    LowDepthFlag = 1,
+    TechnicalFailureFlag = 1
+  )
+)
+h1 <- .HermesData(object[1])
+h2 <- .HermesData(object[2])
+h3 <- .HermesData(object)
+
+# # Create SE for testing ---- 
+# nrows1 <- 200; ncols1 <- 6
+# counts1 <- matrix(runif(nrows1 * ncols1, 1, 1e4), nrows1)
+# colData1 <- DataFrame(Treatment=rep(c("ChIP", "Input"), 3),
+#                       row.names=LETTERS[1:6])
+# se0 <- SummarizedExperiment(assays=SimpleList(counts=counts1),
+#                             colData=colData1)
+# se1 <- SummarizedExperiment(assays=SimpleList(counts=counts1),
+#                             colData=colData1)
+# 
+# # Create a matrix for testing ----
+# matrix1 <- matrix(1L, 2, 2)
+# matrix2 <- matrix(2L, 2, 2)
+
+# rbind ----
+test_that("Test rbind function",{
+  expect_equal(rbind(h1, h2), h3)
+  expect_equal(dim(rbind(h1, h2)), dim(h3))
+  expect_equal(rowData(rbind(h1, h2)), rowData(h3))
+  expect_equal(colData(rbind(h1, h2)), colData(h3))
 })
+
+# test_that("rbind function fails for non-HermesData objects", {
+#   expect_error(rbind(se0, se1), "Not all input objects are HermesData, please check again")
+#   expect_error(rbind(matrix1, matrix2), "Not all input objects are HermesData, please check again")
+# })
