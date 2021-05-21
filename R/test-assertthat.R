e8dc93eeb5843dd541d05fe1272b61bcbc35d485
Stefanie <stefanie.bienert.sb1@roche.com>
2021-05-21 16:25:33 +0200

    Update unit test

--- R/test-assertthat.R
+++ R/test-assertthat.R
@@ -4,14 +4,16 @@ library(testthat)
 
 # Tests
 
-testthat("on_failure(is) error message for input vs class character is correct", {
+testthat("on_failure(is) error message is correct", {
   a <- 5
   expect_error(assert_that(is(a, "character"))
-  , "Error: a is not of class character")
+  , "a is not of class character")
   
-}
-  
-)
+})
+
+
+
+
 a <- 5
 assert_that(is(a, "character"))
 
