2bf44ccd9ac62ae933c8f3c49751cc04d76cf30d
Jeff Luong <jeff.luong@roche.com>
2021-05-28 13:41:27 -0400

    added minor changes and ggplot2 to the imports section of the DESCRIPTION file

--- R/graphs.R
+++ R/graphs.R
@@ -4,14 +4,15 @@
 #'
 #' @param object A [HermesData] object.
 #' @param bins Number of evenly distributed groups desired.
-#' @importFrom ggplot2 ggplot aes geom_histogram
+#' @importFrom ggplot2 ggplot aes geom_histogram stat_bin ggtitle xlab ylab 
 #' @export
 #' @examples
-#' draw_libsize_hist(HermesData, 30)
+#' result <- hermes:::.HermesData(summarized_experiment)
+#' draw_libsize_hist(result, 30)
 #'
-draw_libsize_hist <- function(object, bins, ...){
+draw_libsize_hist <- function(object, bins){
   df <- as.data.frame(colSums(counts(object)))
-  ggplot(df, aes(colSums(counts(result)))) +
+  ggplot(df, aes(colSums(counts(object)))) +
     geom_histogram(color = "white", bins = bins) +
     stat_bin(bins = bins, geom = "text", aes(label = ifelse(..count.. > 0, ..count.., "")), vjust = -0.25) +
     ggtitle("Histogram of Library Sizes") +
