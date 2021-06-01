9dbb84fee39223b5849353eb0c3fa40ce6ed3dc7
Jeff Luong <jeff.luong@roche.com>
2021-06-01 10:24:23 -0400

    added draw_libsize_densities() function to graphs.R

--- R/graphs.R
+++ R/graphs.R
@@ -35,3 +35,36 @@ draw_libsize_hist <- function(object,
     xlab("Library Size") +
     ylab("Frequency")
 }
+
+#' Density Plot of Log2/Count Distribution
+#'
+#' This creates a density plot of the log2/count distribution of the [HermesData] object.
+#'
+#' @param object (`HermesData`)\cr input.
+#' @param log (`logical`)\cr TRUE or FALSE vector for Log2 transformation.
+#' @return The `ggplot` object with the density plot.
+#' 
+#' @export
+#' @examples
+#' result <- HermesData(summarized_experiment)
+#' draw_libsize_densities(result, TRUE)
+#'
+draw_libsize_densities <- function(object,
+                                   log = TRUE){
+  assert_that(
+    is_class(object, "HermesData"),
+    is.logical(log)
+  )
+  if(isTRUE(log)){
+    df <- as.data.frame(log2(counts(object) + 1))
+  } else {
+    df <- as.data.frame(counts(object))
+  }
+  df.long <- gather(df)
+  ggplot(df.long, aes(value, group = key)) +
+    geom_density() +
+    expand_limits(x = -2.5) +
+    ggtitle(ifelse(isTRUE(log), "Log2 Count Distribution", "Count Distribution")) +
+    xlab(ifelse(isTRUE(log), "Log2(Count + 1)", "Counts")) +
+    ylab("Density")
+}
