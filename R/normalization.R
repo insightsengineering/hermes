ec00c7927361997b3d8a4f9451846a3bfe097f80
Lyndsee Z <zhanl238@gene.com>
2021-06-02 00:10:57 +0000

    adding control_normalize

---
+++ R/normalization.R
@@ -0,0 +1,32 @@
+# control_normalize ----
+
+#' Settings for the normalize function
+#' 
+#' @param log logical, if TRUE then log2 values are returned
+#' @param lib_size library size, defaults to colSums(y)
+#' @param prior_count average count to be added to each observation to avoid 
+#'   taking log of zero, used only when log=TRUE
+#'   
+#' @return Values for the settings used to perform the normalization procedure.
+#' 
+#' @note Note, to be used with the normalize function.
+#'   
+#' @export
+#' @examples
+#' control <- control_normalize()
+#' control1 <- control_normalize(log = FALSE, lib_sizes = 1000000L, prior_count = 0.5)
+#'               
+control_normalize <- function(log = TRUE,
+                              lib_sizes = NULL,
+                              prior_count = 1) {
+  assert_that(
+    is.flag(log),
+    is.null(lib_sizes) || utils.nest::is_integer_vector(lib_sizes),
+    is.number(prior_count) && prior_count >= 0
+  )
+  list(
+    log = log,
+    lib_sizes = lib_sizes,
+    prior_count = prior_count
+  )
+}
---
+++ tests/testthat/test-normalization.R
@@ -0,0 +1,12 @@
+# control_normalize ----
+
+test_that("control_normalize function works as expected", {
+  control1 <- control_normalize()
+  control2 <- control_normalize(log = T, lib_sizes = 60000000L, prior_count = 3)
+   
+  expect_is(control1, "list")
+  expect_is(control2, "list")
+  expect_error(control_normalize(log = "a"))
+  expect_error(control_normalize(lib_sizes = 1))
+  expect_error(control_normalize(prior_count = -1))
+})
