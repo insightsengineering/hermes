a8b873352b968d6158eba89fb9c624fc4303d1eb
Jeff Luong <jeff.luong@roche.com>
2021-06-04 09:58:39 -0400

    updated documentation

--- R/normalization.R
+++ R/normalization.R
@@ -37,7 +37,6 @@ control_normalize <- function(log = TRUE,
 #' @return A numeric matrix with normalized counts using the TPM method.
 #'   
 #' @importFrom edgeR rpkm
-#' @importFrom edgeR tpm
 #' @examples
 #' h <- HermesData(summarized_experiment)
 #' cont <- control_normalize()
