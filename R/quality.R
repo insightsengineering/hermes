8bac015ea0aaff97de48550ca1911c95fb7ad3d9
Lyndsee Z <zhanl238@gene.com>
2021-06-04 15:22:26 +0000

    adding control_quality edits

--- R/quality.R
+++ R/quality.R
@@ -1,16 +1,18 @@
 #' Control for Specified Quality Flags
 #' 
-#' @param min_cpm (`number`)\cr minimum cpm for each gene within the sample to pass, default is 1.
+#' @param min_cpm (`number`)\cr minimum CPM for each gene within the sample to pass.
 #' @param min_readcount_prop (`proportion`)\cr minimum percentage of samples with acceptable read count of certain gene, default is 0.25.
-#' @param min_corr (`proportion`)\cr minimum correlation of RNAseq results, default is 0.5.
-#' @param min_depth (`number`)\cr minimum library depth, default is \code{NULL}, which means lower quartile minus 1.5 times 
-#'   of the interquartile range is used as the threshold. 
+#' @param min_corr (`proportion`)\cr minimum correlation of RNAseq results.
+#' @param min_depth (`number`)\cr minimum library depth. 
 #'   
 #' @return List with the above criteria to flag observations.
 #' 
 #' @note To be used with the `add_quality_flags()` function.
-#'   
+#'
+#' @importFrom tern is_proportion
+#' @importFrom tern is_nonnegative_count
 #' @export
+#' 
 #' @examples
 #' control_quality()
 #' control_quality(min_cpm = 5, min_readcount_prop = .001, min_corr = .1, min_depth = 3)
