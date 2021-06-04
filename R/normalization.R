bf7110f0d929ba285b258cd872f0f9ff17c1153c
Jeff Luong <jeff.luong@roche.com>
2021-06-04 10:34:14 -0400

    added h_voom code to normalization.R

--- R/normalization.R
+++ R/normalization.R
@@ -29,3 +29,36 @@ control_normalize <- function(log = TRUE,
     prior_count = prior_count
   )
 }
+#' Control Settings for Counts Normalization
+#' 
+#' @param log (`flag`)\cr whether `log2` values are returned, otherwise original scale is used.
+#' @param lib_sizes (`numeric`)\cr library sizes, the default is the vector with the sum of the
+#'   counts for each of the samples.
+#' @param prior_count (`count`)\cr average count to be added to each observation to avoid 
+#'   taking log of zero, used only when `log = TRUE`.
+#'   
+#' @return List with the above settings used to perform the normalization procedure.
+#' 
+#' @note To be used with the `normalize()` function.
+#'   
+#' @export
+#' @examples
+#' counts_voom <- h_voom(h, cont)
+#' str(counts_voom)
+#'               
+h_voom <- function(object, 
+                   control) {
+  assert_that(
+    is_hermes_data(object),
+    utils.nest::is_fully_named_list(control)
+  )
+  norm_log2 <- limma::voom(
+    counts = counts(object),
+    lib.size = control$lib_sizes
+  )$E
+  if (control$log) {
+    norm_log2
+  } else {
+    2^norm_log2
+  }
+}
