9b929aa072038a2c51307c6d51bf2e2904614c3d
Jeff Luong <jeff.luong@roche.com>
2021-06-14 09:20:56 -0400

    updated with Daniel's suggestions

--- data-raw/create_mae.R
+++ data-raw/create_mae.R
@@ -1,47 +1,50 @@
 set.seed(100)
 
 #Starting with the example SE object
-object <- summarized_experiment
+se <- summarized_experiment
+
+#Randomly fill the counts assay
+assay(se, "counts")[] <- rnbinom(n = prod(dim(se)), size = 1000, prob = 0.5)
 
 #Subset the example SE object into 3 different SEs
-object1 <- object[1000:2000, 1:5]
-object2 <- object[1000:3500, 6:14]
-object3 <- object[1500:2800, 15:20]
+se1 <- se[1000:2000, 1:5]
+se2 <- se[1000:3500, 6:14]
+se3 <- se[1500:2800, 15:20]
 
 #Randomly adding NAs into the counts of the 3 SE objects
-counts_matrix1 <- assays(object1)$counts
+counts_matrix1 <- assays(se1)$counts
 indices1 <- arrayInd(sample(length(counts_matrix1), length(counts_matrix1)*0.1), dim(counts_matrix1))
 counts_matrix1[indices1] <- NA
 
-counts_matrix2 <- assays(object2)$counts
+counts_matrix2 <- assays(se2)$counts
 indices2 <- arrayInd(sample(length(counts_matrix2), length(counts_matrix2)*0.15), dim(counts_matrix2))
 counts_matrix2[indices2] <- NA
 
-counts_matrix3 <- assays(object3)$counts
+counts_matrix3 <- assays(se3)$counts
 indices3 <- arrayInd(sample(length(counts_matrix3), length(counts_matrix3)*0.25), dim(counts_matrix3))
 counts_matrix3[indices3] <- NA
 
 #Create sample maps for each experiment
-object1map <- data.frame(
+se1map <- data.frame(
   primary = c("Jack", "Jill", "Barbara", "Bob", "John"),
   colname = c("06520011B0023R", "06520067C0018R", "06520063C0043R", "06520105C0017R", "06520092C0017R"),
   stringsAsFactors = FALSE
 )
 
-object2map <- data.frame(
+se2map <- data.frame(
   primary = c("Jack", "Jill", "Barbara", "Bob", "John", "Jane", "Claire", "Mike", "Kate"),
   colname = c("06520103C0017R", "06520001B0023R", "06520022C0017R", "06520062C0017R", "06520046C0018R", 
               "06520101B0017R", "06520047C0017R", "06520024B0014R", "06520080B0023R"),
   stringsAsFactors = FALSE
 )
 
-object3map <- data.frame(
+se3map <- data.frame(
   primary = c("Jack", "Jill", "Barbara", "Bob", "John", "Jane"),
   colname = c("06520093C0017R", "06520070C0018R", "06520023C0018R", "06520099B0017R", "06520015C0016R", "06520019C0023R"),
   stringsAsFactors = FALSE
 )
 
-maplist <- list(object1 = object1map, object2 = object2map, object3 = object3map)
+maplist <- list(se1 = se1map, se2 = se2map, se3 = se3map)
 sampMap <- listToMap(maplist)
 
 #Create an example phenotype data
@@ -50,7 +53,7 @@ colDat <- data.frame(sex = c("M", "F", "F", "M", "M", "F", "F", "M", "F"),
                      row.names = c("Jack", "Jill", "Barbara", "Bob", "John", "Jane", "Claire", "Mike", "Kate"))
 
 #Create a named experiment list
-assaylist <- list(object1 = object1, object2 = object2, object3 = object3)
+assaylist <- list(se1 = se1, se2 = se2, se3 = se3)
 ExpList <- ExperimentList(assaylist)
 
 #Create a MultiAssayExperiment object
