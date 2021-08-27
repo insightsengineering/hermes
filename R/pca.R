625c6de2a9cef2a78c228dba0226d10aa19802db
Stefanie Bienert <75780729+bienerts@users.noreply.github.com>
2021-08-27 13:03:44 +0200

    add filter function to pca and tests (#41)
    * add filter function to pca and tests
    
    * Update tests/testthat/test-pca.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * updates to NEWS, documentation and test
    
    * Update R/pca.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update R/pca.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update tests/testthat/test-pca.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update test-pca.R
    
    * Update test-pca.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>

--- R/pca.R
+++ R/pca.R
@@ -19,6 +19,7 @@
 #'
 #' @param object (`AnyHermesData`) \cr input.
 #' @param assay_name (`string`) \cr name of the assay to use.
+#' @param n_top (`count` or `NULL`)\cr filter criteria based on number of genes with maximum variance.
 #'
 #' @return A [HermesDataPca] object which is an extension of the [stats::prcomp] class.
 #'
@@ -33,9 +34,11 @@
 #'   filter() %>%
 #'   normalize()
 #'
-#' # Perform PCA.
 #' result <- calc_pca(object, assay_name = "tpm")
 #' summary(result)
+#'
+#' result1 <- calc_pca(object, assay_name = "tpm", n_top = 500)
+#' summary(result1)
 #'
 #' # Plot the results.
 #' autoplot(result)
@@ -43,13 +46,27 @@
 #' autoplot(result, variance_percentage = FALSE)
 #' autoplot(result, label = TRUE, label.repel = TRUE)
 calc_pca <- function(object,
-                     assay_name = "counts") {
+                     assay_name = "counts",
+                     n_top = NULL) {
   assert_that(
     is_hermes_data(object),
-    is.string(assay_name)
+    is.string(assay_name),
+    is.numeric(n_top) || is.null(n_top)
   )
 
-  x_samples <- assay(object, assay_name)
+  x_samples_filtered <- if (is.null(n_top)) {
+    object
+  } else {
+    top_var <- top_genes(
+      object = object,
+      assay_name = assay_name,
+      summary_fun = rowVars,
+      n_top = n_top
+    )
+    subset(object, subset = GeneID %in% as.character(top_var$name))
+  }
+
+  x_samples <- assay(x_samples_filtered, assay_name)
   x_genes <- t(x_samples)
   gene_is_constant <- apply(x_genes, MARGIN = 2L, FUN = S4Vectors::isConstant)
   x_genes_remaining <- x_genes[, !gene_is_constant]
