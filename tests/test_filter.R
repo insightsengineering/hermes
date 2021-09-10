d556fe412ef0486e60cd2e1aadff9ca9f70da78f
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2021-09-10 12:16:49 +0200

    Add documentation and comments.

--- tests/test_filter.R
+++ tests/test_filter.R
@@ -1,3 +1,5 @@
+# In order to ensure that `hermes` does not make `dplyr` filter method unusable,
+# we have this separate test as we need to first load `dplyr` and then `hermes`.
 library(dplyr)
 library(hermes)
 filter(iris, Species == "setosa")
