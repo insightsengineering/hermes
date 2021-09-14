2e58b46d4f0dd4a5767d4bcb31bd2590a425195d
zhanl238 <zhanl238@gene.com>
2021-09-14 18:24:59 -0500

    adding doc and tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -38,6 +38,46 @@ test_that("df_char_to_factor works as expected with custom settings", {
   expect_identical(result, expected)
 })
 
+# df_cols_to_factor ----
+
+test_that("df_cols_to_factor works as expected with default settings", {
+  dat <- S4Vectors::DataFrame(
+    a = c(NA, "B"),
+    b = array(1:4, c(2, 1, 2)),
+    c = c(TRUE, FALSE),
+    d = factor(c("X", NA)),
+    e = c("U", NA)
+  )
+  result <- df_cols_to_factor(dat)
+  expected <- S4Vectors::DataFrame(
+    a = factor(c("<Missing>", "B"), levels = c("B", "<Missing>")),
+    b = array(1:4, c(2, 1, 2)),
+    c = factor(c(TRUE, FALSE)),
+    d = factor(c("X", NA)),
+    e = factor(c("U", "<Missing>"), levels = c("U", "<Missing>"))
+  )
+  expect_identical(result, expected)
+})
+
+test_that("df_cols_to_factor works as expected with custom settings", {
+  dat <- S4Vectors::DataFrame(
+    a = c(NA, "B"),
+    b = array(1:4, c(2, 1, 2)),
+    c = c(TRUE, FALSE),
+    d = factor(c("X", NA)),
+    e = c("U", NA)
+  )
+  result <- df_cols_to_factor(dat, omit_columns = c("a", "b"), na_level = "Misses")
+  expected <- S4Vectors::DataFrame(
+    a = c(NA, "B"),
+    b = array(1:4, c(2, 1, 2)),
+    c = factor(c(TRUE, FALSE)),
+    d = factor(c("X", NA)),
+    e = factor(c("U", "Misses"), levels = c("U", "Misses"))
+  )
+  expect_identical(result, expected)
+})
+
 # h_short_list ----
 
 test_that("h_short_list works as expected with default arguments", {
